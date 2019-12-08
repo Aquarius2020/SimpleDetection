551d9b20b21ac7468bb7440571374915edbb57b2
pprp <1115957667@qq.com>
2019-12-08 21:48:59 +0800

    update simple Detection

---
+++ config/cfg.py
@@ -0,0 +1,25 @@
+import os
+
+SAVE_DIR = './weights'
+
+SAVE_NAME = "new.pth"
+
+TOTAL_EPOCH = 60
+
+BATCH_SIZE = 2
+
+LEARNING_RATE = 3e-4
+
+IMAGE_DIR_PATH = "./data/modified_jpgs/JPEGImages"
+
+DIR_PATH = "./data/modified_jpgs/JPEGImages/"
+
+ANNOTATION_DIR_PATH = "./data/modified_jpgs/Annotations"
+
+NUM_OF_IMAGES = len(os.listdir(IMAGE_DIR_PATH))
+
+CLASS_NAME = ['background', 'keyPoint_1', 'keyPoint_2']
+
+id2className_dict = {a: b for a, b in enumerate(CLASS_NAME)}
+
+className2id_dict = {b: a for a, b in enumerate(CLASS_NAME)}
